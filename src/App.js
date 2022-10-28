a71169649752149649a2aeaf34ffb68e579090ad
lbcanfield <lewie.b.canfield@gmail.com>
2022-10-28 15:12:15 -0500

    Skipping over likePost component in App.js until it is called.  Rendered the SearchBar component from the SearchBar.js file.  Does not appear to have any named props passed to it.

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
 
-  const likePost = postId => {
+  const likePost = postId => {  /////////////////////  RETURN TO THIS WHEN IT IS CALLED FOR
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -36,6 +36,8 @@ const App = () => {
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      {/* Calling the SearchBar component from SearchBar.js */}
+      <SearchBar />  {/*SearchBar doesn't appear to have any requred props passed to it other than props*/}
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
